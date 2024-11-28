9f54ecb85b917d17c07f6c2ec362eb5ecc29b0b0
Novin <cocolupiayi@gmail.com>
2024-11-28 13:11:19 +0800

    add simple sprite

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,28 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
-int main()
-{
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+int main() {
+  auto window =
+      sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
+  window.setFramerateLimit(144);
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-            {
-                window.close();
-            }
-        }
+  sf::Texture texture;
 
-        window.clear();
-        window.display();
+  if (!texture.loadFromFile("images/charmander.png")) {
+    std::cout << "File not found" << std::endl;
+  }
+
+  sf::Sprite sprite(texture);
+  
+  while (window.isOpen()) {
+    while (const std::optional event = window.pollEvent()) {
+      if (event->is<sf::Event::Closed>()) {
+        window.close();
+      }
     }
+
+    window.clear();
+    window.draw(sprite);
+    window.display();
+  }
 }
---
+++ src/sprite.hpp
@@ -0,0 +1,14 @@
+#ifndef SPRITE_HPP
+#define SPRITE_HPP
+
+#include "SFML/Graphics/Sprite.hpp"
+#include "SFML/Graphics/Texture.hpp"
+
+class Sprite {
+public:
+  sf::Texture texture;
+  sf::Sprite sprite;
+private:
+};
+
+#endif
